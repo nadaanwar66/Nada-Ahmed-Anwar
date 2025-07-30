8fb87cb5fd18d05c40aace25288aaa456df76ef9
nadaanwar66 <ozzlaly1@gmail.com>
2025-07-30 04:08:18 +0300

    Add nothing

--- task/study.py
+++ task/study.py
@@ -1,5 +1,3 @@
-rows=int(input("Enter the number of rows:\n"))
-for i in range(1,rows,+1):
-    spaces=" "*(rows-i)
-    stars="*"*(2*i-1)
-    print(spaces+stars)
+def say_my_name(name):
+    print("hello",name)
+    say_my_name("samir")
